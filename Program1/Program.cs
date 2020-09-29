f7da4ebf2b57a511b6b340ce6c66762d8a243bf2
Ruslan Boom <rusik.lim@yandex.ru>
2020-09-30 01:17:28 +0300

    Добавьте файлы проекта.

---
+++ Program1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Средний
+            try
+            {
+                double N, m, y;
+                Console.Write("введите m:");
+                m = double.Parse(Console.ReadLine());
+                Console.Write("введите y:");
+                y = double.Parse(Console.ReadLine());
+                N = (m * m + 2.8 * m + 0.355) / (Math.Cos(2 * y) + 3.6);
+                Console.WriteLine("N=" + N);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
